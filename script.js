970b6cb13153a0447c17ecb5a023afa44d3f6440
Phu Dinh <55571245+phudinhtruongk18@users.noreply.github.com>
2021-06-02 22:58:47 +0700

    Make by Thank Long

---
+++ script.js
@@ -0,0 +1,20 @@
+const img_one = document.querySelector('.user01');
+const img_two = document.querySelector('.user02');
+const cake = document.querySelector('.cake');
+new Audio("sound.mp3").play()
+
+img_one.setAttribute('style', 'display: none;');
+img_two.setAttribute('style', 'display: none;');
+cake.setAttribute('style', 'display: none;');
+
+setTimeout(() => {
+    img_one.setAttribute('style', 'display: unset;');
+}, 2000);
+
+setTimeout(() => {
+    img_two.setAttribute('style', 'display: unset;');
+}, 6000);
+
+setTimeout(() => {
+    cake.setAttribute('style', 'display: unset;');
+}, 9000);
