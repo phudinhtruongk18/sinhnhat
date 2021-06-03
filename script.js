3df1c7947c0c7724b88928e4694671d026ed2de0
Phu Dinh <you@>
2021-06-03 10:05:13 +0700

    cap nhap

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const img_one = document.querySelector('.user01');
 const img_two = document.querySelector('.user02');
 const cake = document.querySelector('.cake');
 
-var sound = new Audio("sound.mp3");
+var sound = new Audio();
 sound.play();
-sound.addEventListener('ended', () => sound.play());
+sound.addEventListener('ended', () => {
+    sound.src = "sound.mp3"
+    sound.play());
+};
 
 img_one.setAttribute('style', 'display: none;');
 img_two.setAttribute('style', 'display: none;');
